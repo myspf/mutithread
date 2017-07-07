79af5c6ca55f173dc97eccae8fced37d7344ef5d
Administrator <administrator@ubuntu.ubuntu-domain>
2017-07-08 07:19:00 +0800

    new file

---
+++ main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <unistd.h>
+#include<sys/types.h>
+#include<pthread.h>
+#include<stdlib.h>
+using namespace std;
+void printids(const char * s){
+    pid_t pid;
+    pthread_t tid;
+    pid = getpid();
+    tid = pthread_self();
+    cout<<s<<" pid is :"<<pid<<", tid is :"<<tid<<endl;
+}
+
+void *thr_fn(void *arg){
+    printids("new thread :");
+    return (void *)0;
+}
+
+int main(){
+pthread_t ntid;
+int err;
+err = pthread_create(&ntid,NULL,thr_fn,NULL);
+if(err != 0)
+    cout<<"create thread fail , the err is "<<err<<endl;
+printids("main thread:");
+sleep(1);
+exit(0);
+}
---
+++ main2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <unistd.h>
+#include<sys/types.h>
+#include<pthread.h>
+#include<stdlib.h>
+using namespace std;
+
+void * fun1(void *arg){
+    cout<<"thread 1 return "<<endl;
+    return (void *)1;
+}
+void * fun2(void *arg){
+    cout<<"thread 2 return "<<endl;
+    return (void *)2;
+}
+
+int main(){
+    int err;
+    pthread_t tid1,tid2;
+    void *ret;
+    pthread_create(&tid1,NULL,fun1,NULL);
+    pthread_create(&tid2,NULL,fun2,NULL);
+    pthread_join(tid1,&ret);
+    cout<<"thred 1 exit:"<<reinterpret_cast<int>(ret)<<endl;
+    pthread_join(tid2,&ret);
+    cout<<"thred 2 exit:"<<reinterpret_cast<int>(ret)<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
