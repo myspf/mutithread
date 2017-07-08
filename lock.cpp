d4542f85e64db91e391fa952b30f76e449a931f3
Administrator <administrator@ubuntu.ubuntu-domain>
2017-07-08 21:47:14 +0800

    add lock

---
+++ lock.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <unistd.h>
+#include<sys/types.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include <fstream>
+using namespace std;
+int global;
+class Mutex{
+public:
+	Mutex()
+	{
+		pthread_mutex_init(&m_,NULL);
+	}
+	~Mutex()
+	{
+		pthread_mutex_destroy(&m_);	
+	}
+	void lock()
+	{
+		pthread_mutex_lock(&m_);
+	}
+	void unlock()
+	{
+		pthread_mutex_unlock(&m_);
+	}
+
+private:
+	pthread_mutex_t m_;
+};
+
+Mutex m;
+class Info{
+public:
+	Info(){
+		for(int i = 0 ;i<10;i++)
+			a[i] = 0;
+	}
+	int a[10];
+};
+Info info; 
+void *f1(void *arg){
+		while(1){
+	for(int i = 0 ;i < 10 ;i++){
+		info.a[i] = i*30;	
+	}
+	
+	}
+
+return (void *)0;
+}
+
+void *f2(void *arg){
+	while(1){
+	for(int i = 0 ;i < 10 ;i++){
+		info.a[i] = i*30;	
+	}
+	
+}
+return (void *)0;
+}
+
+int main(){
+	pthread_t t1,t2;
+	void *ret;
+	pthread_create(&t1,NULL,f1,NULL);
+	pthread_create(&t2,NULL,f2,NULL);
+	pthread_join(t1,&ret);
+	pthread_join(t2,&ret);
+	cout<<"global is :"<<global<<endl;
+	return 0;
+}
